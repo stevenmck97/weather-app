a2c26ab83dc32e67e861797ba366feab5d336ed2
Steven McKeown <stevenmckeown19@gmail.com>
2022-04-19 15:17:24 +0100

    Added function to display weather data

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ async function getWeather(location) {
             `http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${weather_key}`
         );
         const weatherData = await response.json();
+        displayWeather(weatherData);
         console.log(weatherData);
     } catch (error) {
         console.log(error);
@@ -18,4 +19,28 @@ async function getWeather(location) {
 function getUserLocationInput() {
     let location = locationInput.value;
     getWeather(location);
+}
+
+function displayWeather(weatherData) {
+    const weatherDisplay = document.querySelector(".weatherDisplay");
+    const weatherDescription = document.querySelector("#weatherDescription");
+    const location = document.querySelector("#location");
+    const temp = document.querySelector("#temp");
+    const feelsLikeTemp = document.querySelector("#feelsLikeTemp");
+    const wind = document.querySelector("#wind");
+    const humidity = document.querySelector("#humidity");
+
+    weatherDescription.textContent = weatherData.weather.description;
+    location.textContent = weatherData.name;
+    temp.textContent = weatherData.main.temp;
+    feelsLikeTemp.textContent = weatherData.main.feels_like;
+    wind.textContent = weatherData.wind.speed;
+    humidity.textContent = weatherData.main.humidity;
+
+    weatherDisplay.appendChild(weatherDescription);
+    weatherDisplay.appendChild(location);
+    weatherDisplay.appendChild(temp);
+    weatherDisplay.appendChild(feelsLikeTemp);
+    weatherDisplay.appendChild(wind);
+    weatherDisplay.appendChild(humidity);
 }
